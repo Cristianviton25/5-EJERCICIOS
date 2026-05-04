71e96d33e4ac4508733b513c1d841cae5d67b050
Vitonco <cristianviton25@gmail.com>
2026-05-04 11:05:25 -0500

    ojala funcione

--- test/app.test.js
+++ test/app.test.js
@@ -9,6 +9,39 @@ function ejecutarPruebas () {
         console.log("🟩 test 1 pasado: función ejercicio 13 correcta");
         pasadas ++;
     } else {
-        console.log("🟥 test 1 fallido:", eje13)
+        console.log("🟥 test 1 fallido:", eje13);
+        fallidas++;
     }
+
+    const eje14 = ejercicio14("Resuelve")
+    if (eje14.includes("Resuelve")){
+        console.log("🟩 test 2 pasado: función ejercicio 14 correcta");
+        pasadas ++;
+    } else {
+        console.log("🟥 test 2 fallido:", eje14);
+        fallidas ++;
+    }
+
+    const eje15 = ejercicio15("Resuel")
+    if (eje15.includes("Resuel")){
+        console.log("🟩 test 3 pasado: función ejercicio 15 correcta");
+        pasadas ++;
+    } else {
+        console.log("🟥 test 3 fallido:", eje15);
+        fallidas ++;
+    }
+
+    const eje16 = ejercicio14("Resu")
+    if (eje16.includes("Resu")){
+        console.log("🟩 test 4 pasado: función ejercicio 16 correcta");
+        pasadas ++;
+    } else {
+        console.log("🟥 test 4 fallido:", eje16);
+        fallidas ++;
+    }
+
+    console.log("\nResultados: " + pasadas + " pasadas, ", + fallidas + "fallidas");
+    if (fallidas > 0) process.exit(1);
 }
+
+ejecutarPruebas();
