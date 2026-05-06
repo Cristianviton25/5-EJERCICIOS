606755a66a710fd6a6fa043b8374aa40ff5064a7
Vitonco <cristianviton25@gmail.com>
2026-05-06 18:23:53 -0500

    probando

--- src/js/ejercicio14.js
+++ src/js/ejercicio14.js
@@ -1,10 +1,12 @@
-// ✅ LÓGICA
+// ✅ FUNCIÓN PARA TEST
 export function ejercicio14(input) {
+
     if (!input) {
         return "Ingresa números válidos.";
     }
 
     const array = input.split(",").map(num => num.trim());
+
     const invertido = [];
 
     for (let i = array.length - 1; i >= 0; i--) {
@@ -15,11 +17,14 @@ export function ejercicio14(input) {
 }
 
 
-// ✅ DOM (para tu página)
-export function ejercicio14HTML() {
+// ✅ FUNCIÓN PARA EL NAVEGADOR
+export function reverseArray() {
+
+    // evita error en npm test
     if (typeof document === "undefined") return;
 
     const input = document.getElementById("arrayInput14").value;
+
     const resultado = ejercicio14(input);
 
     document.getElementById("resultado").innerHTML = resultado;
